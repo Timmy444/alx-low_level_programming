2d09f2b8a287974fab7437377976c5f10d439d1c
Timmy444 <rodiatomofoye8@gmail.com>
2023-03-08 02:03:22 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include "main.h"
+
 /**
- * _puts_recursion - functions like puts();
- * @c: input
- * Return: Always 0 (Success)
+ *_puts_recursion - prints a string
+ *@s: pointer block of memory to fill
+ *Return: void
  */
+
 void _puts_recursion(char *s)
+
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	       	_putchar('\n');
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_putchar (*s);
+_puts_recursion(s + 1);
+
 }
